2ff91b87bfc1d697b04d0f4ba54c5fe79ebc51f8
David Luo <davidkluo@gmail.com>
2023-02-27 16:21:02 -0800

    Complete 242-valid-anagram

---
+++ 242-valid-anagram.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+function isAnagram(s, t) {
+  let letterFreqs = {};
+
+  for (let char of s) {
+    letterFreqs[char] = (letterFreqs[char] || 0) + 1;
+  }
+
+  for (let char of t) {
+    if (letterFreqs[char] === 0) return false;
+
+    letterFreqs[char] -= 1;
+  }
+
+  for (let char in letterFreqs) {
+    if (letterFreqs[char] !== 0) return false;
+  }
+
+  return true;
+};

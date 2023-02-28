ca97303b45de94d19bb196d8ddafb5c9b3aa0bac
David Luo <davidkluo@gmail.com>
2023-02-27 17:07:39 -0800

    Complete 733-flood-fill

---
+++ 733-flood-fill.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+function floodFill(image, sr, sc, color) {
+  const scolor = image[sr][sc];
+  const maxRow = image.length - 1;
+  const maxCol = image[0].length - 1;
+
+  if (scolor === color) return image;
+
+  function _floodFillDFS(row, col, color) {
+    if (image[row][col] !== scolor) return;
+
+    image[row][col] = color;
+
+    if (row > 0) _floodFillDFS(row-1, col, color);
+
+    if (row < maxRow) _floodFillDFS(row+1, col, color);
+
+    if (col > 0) _floodFillDFS(row, col-1, color);
+
+    if (col < maxCol) _floodFillDFS(row, col+1, color);
+  }
+
+  _floodFillDFS(sr, sc, color);
+  return image;
+};

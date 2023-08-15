6df6f01935326dedba78c81c3324aa0647768a57
David Luo <davidkluo@gmail.com>
2023-08-15 09:29:56 -0700

    Complete 139 word break

---
+++ medium/139-word-break.ts
@@ -0,0 +1,27 @@
+function wordBreak(s: string, wordDict: string[]): boolean {
+  const wordDictSet = new Set(wordDict);
+
+  const queue: number[] = [0];
+  const seen = new Set<number>(queue);
+
+  while (queue.length > 0) {
+    const start: number = queue.shift()!;
+
+    if (start === s.length) {
+      return true;
+    }
+
+    for (let end = start + 1; end <= s.length; end++) {
+      if (seen.has(end)) {
+        continue;
+      }
+
+      if (wordDictSet.has(s.slice(start, end))) {
+        seen.add(end);
+        queue.push(end);
+      }
+    }
+  }
+
+  return false;
+}

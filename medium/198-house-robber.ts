6a98b8b79a2434e8cf0458642d85e2878bfde309
David Luo <davidkluo@gmail.com>
2023-08-23 15:24:41 -0700

    Add alternative solution to 198 house robber with better memory usage

--- medium/198-house-robber.ts
+++ medium/198-house-robber.ts
@@ -1,10 +1,26 @@
-function rob(nums: number[]): number {
-  const dp = [nums[0]];
+// function rob(nums: number[]): number {
+//   const dp = [nums[0]];
 
-  for (let i = 1; i < nums.length; i++) {
-    const money = nums[i];
-    dp[i] = Math.max(dp[i - 1], i - 2 >= 0 ? dp[i - 2] + money : money);
+//   for (let i = 1; i < nums.length; i++) {
+//     const money = nums[i];
+//     dp[i] = Math.max(dp[i - 1], i - 2 >= 0 ? dp[i - 2] + money : money);
+//   }
+
+//   return dp[nums.length - 1];
+// }
+
+// Solution with O(1) space
+function rob(nums: number[]): number {
+  if (nums.length === 1) return nums[0];
+
+  let rob1 = 0;
+  let rob2 = 0;
+
+  for (const num of nums) {
+    const money = Math.max(rob1 + num, rob2);
+    rob1 = rob2;
+    rob2 = money;
   }
 
-  return dp[nums.length - 1];
+  return rob2;
 }

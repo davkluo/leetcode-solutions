f9489bf82d53dad092e9b8215eebb75e077b4e14
David Luo <davidkluo@gmail.com>
2023-08-28 12:12:40 -0700

    Revise documentatino for 213 house robber ii

--- medium/213-house-robber-ii.ts
+++ medium/213-house-robber-ii.ts
@@ -1,42 +1,32 @@
-// function rob2(nums: number[]): number {
-//   if (nums.length === 1) return nums[0];
-
-//   const dp1 = [nums[0]]; // Always steals from first house, never last
-//   for (let i = 1; i < nums.length - 1; i++) {
-//     const money = nums[i];
-//     dp1[i] = Math.max(dp1[i - 1], i - 2 >= 0 ? dp1[i - 2] + money : money);
-//   }
-
-//   const dp2 = [0, nums[1]]; // Always steals from second house, possibly last
-//   for (let i = 2; i < nums.length; i++) {
-//     const money = nums[i];
-//     dp2[i] = Math.max(dp2[i - 1], i - 2 >= 1 ? dp2[i - 2] + money : money);
-//   }
-
-//   return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1]);
-// }
-
-// Alternative using helper based on rob() and O(1) space
-function helper(nums: number[]): number {
+/** Given an integer array representing the amount of money at each house,
+ *  returns the maximum amount of money that can be robbed if no two adjacent
+ *  houses can be robbed. The houses are arranged in a circle.
+ *
+ * @param {number[]} nums
+ * @returns {number}
+ */
+function rob(nums: number[]): number {
   if (nums.length === 1) return nums[0];
 
-  let rob1 = 0;
-  let rob2 = 0;
+  function helper(nums: number[]): number {
+    if (nums.length === 1) return nums[0];
 
-  for (const num of nums) {
-    const money = Math.max(rob1 + num, rob2);
-    rob1 = rob2;
-    rob2 = money;
+    let rob1 = 0;
+    let rob2 = 0;
+
+    for (const num of nums) {
+      const money = Math.max(rob1 + num, rob2);
+      rob1 = rob2;
+      rob2 = money;
+    }
+
+    return rob2;
   }
-
-  return rob2;
-}
-
-function rob2(nums: number[]): number {
-  if (nums.length === 1) return nums[0];
 
   return Math.max(
     helper(nums.slice(0, nums.length - 1)),
     helper(nums.slice(1))
   );
 }
+
+// Time: O(n), Space: O(1)

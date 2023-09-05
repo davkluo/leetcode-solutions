6d6a321eac39c00159009c4f6e3a260c9a280ed2
David Luo <davidkluo@gmail.com>
2023-09-05 15:42:07 -0700

    Complete 746 min cost climbing stairs in TS

---
+++ easy/746-min-cost-climbing-stairs.ts
@@ -0,0 +1,23 @@
+/**
+ * Given an array of costs to climb stairs, and climbing 1 or 2 steps each time,
+ * returns the minimum cost to reach the top floor
+ *
+ * Note: 2 <= cost.length <= 1000
+ *
+ * @param {number[]} cost
+ * @returns {number}
+ */
+function minCostClimbingStairs(cost: number[]): number {
+  const top = cost.length;
+
+  let twoBehindCost = cost[0];
+  let oneBehindCost = cost[1];
+
+  for (let i = 2; i < top; i++) {
+    const tmp = oneBehindCost;
+    oneBehindCost = Math.min(oneBehindCost, twoBehindCost) + cost[i];
+    twoBehindCost = tmp;
+  }
+
+  return Math.min(oneBehindCost, twoBehindCost);
+}

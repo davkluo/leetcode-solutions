f97e8bdf8f64ad99269d1bc8dfc73dca57e0254c
David Luo <davidkluo@gmail.com>
2023-09-06 15:52:11 -0700

    Complete 104 maximum depth of binary tree in TS

---
+++ easy/104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/**
+ * Given the root of a binary tree, returns its max depth
+ *
+ * @param {TreeNode | null} root
+ * @returns {number}
+ */
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+
+// Time: O(n), Space: O(log n) on average, O(n) worst case for unbalanced tree

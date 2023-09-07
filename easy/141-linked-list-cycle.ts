842b0913ed79cdb733206487179f322031d48e60
David Luo <davidkluo@gmail.com>
2023-09-07 16:43:03 -0700

    Complete 141 linked list cycle in TS

---
+++ easy/141-linked-list-cycle.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  if (!head) return false;
+
+  let slow = head;
+  let fast = head;
+
+  while (slow?.next && fast?.next?.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Time: O(n), Space: O(1)

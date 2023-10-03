87148eaa304025855320e89969414a242bfd51bd
David Luo <davidkluo@gmail.com>
2023-10-03 16:42:20 -0700

    Add documentation to 268 missing number solution

--- easy/268-missing-number.ts
+++ easy/268-missing-number.ts
@@ -1,3 +1,10 @@
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * returns the only number in the range that is missing from the array.
+ *
+ * @param {number[]} nums
+ * @returns {number}
+ */
 function missingNumber(nums: number[]): number {
   const n = nums.length;
   const targetSum = (n * (n + 1)) / 2;
@@ -6,3 +13,5 @@ function missingNumber(nums: number[]): number {
 
   return targetSum - sum;
 }
+
+// Time: O(n), Space: O(1)
